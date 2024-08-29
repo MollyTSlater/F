1efc9d1851477cf8adaf37e0b9849edfb95823d2
MollyTSlater <mollytslater@gmail.com>
2024-08-29 11:35:12 -0400

    Practice

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,12 @@
 public class Fibonacci {
 
 	public static void main(String[] args) { 
+		nit: change variable name
 		int nthFib = Integer.parseInt(args[0]);
 		System.out.println(new Fibonacci().findNthFib(nthFib));
 	}
 
+	nit: comment needed
 	private int findNthFib(int nthFib) {
 		int currentFib = 1;
 		int prevFib = 0;
